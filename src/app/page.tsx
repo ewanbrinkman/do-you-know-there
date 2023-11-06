6f64144f8b1c4ace3759a31073acbd36a071aed5
Ewan Brinkman <ewanbrinkman@gmail.com>
2023-11-05 22:31:15 -0800

    feat: display home page title in middle of screen
    Display the title of the home page in the middle of the screen.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import MapArea from '@typings/data/MapArea';
 import MapData from '@typings/data/MapData';
 import loadMapData from '@utils/loaders/loadMapData';
 import dynamic from 'next/dynamic';
+import Link from 'next/link';
 const Map = dynamic(() => import('@components/Common/Map'), {
     loading: () => <p>Map is loading...</p>,
     ssr: false,
@@ -29,6 +30,15 @@ const Home: React.FC = () => {
                 alignItems: 'center',
             }}
         >
+            <div className='absolute z-10'>
+                <h1 className='' style={{ fontSize: '3rem' }}>Do You Know {area}</h1>
+                <Link
+                    className="text-blue-500 hover:text-blue-700 text-lg"
+                    href="/play"
+                >
+                    Play
+                </Link>
+            </div>
             <Map mapData={mapData} className="w-full h-full z-0" />
         </div>
     );
