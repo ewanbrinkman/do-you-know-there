bac72d4f185f765c0629cd2175a112c004a78a38
Ewan Brinkman <ewanbrinkman@gmail.com>
2023-11-05 19:16:37 -0800

    chore: test rendering image popup from component
    Try to render the image in a popup from the LocationImage component. It
    looks like renderToStaticMarkup should be used, except that has been
    replaced with renderToPipeableStream. However, an error happens when
    importing renderToPipeableStream. In general, this error and other
    errors have been reported within the past week. It seems like this is
    because of Next.js upgrading to version 14. I think it would be good to
    revisit this later, after breaking changes will of had time to be fixed.

--- src/app/components/Features/Game/GameMap/GameMap.tsx
+++ src/app/components/Features/Game/GameMap/GameMap.tsx
@@ -1,7 +1,11 @@
-'use client';
 import 'leaflet/dist/leaflet.css';
 import React from 'react';
-// import ReactDom from 'react-dom';
+// import ReactDOMServer from "react-dom/server";
+// import { renderToPipeableStream } from 'react-dom/server';
+// import { Writable } from 'stream';
+// import { renderToStaticMarkup } from "react-dom/server";
+// import ReactDOM from "react-dom";
+// import { createPortal } from "react-dom";
 import L from 'leaflet';
 import { useMapEvents } from 'react-leaflet';
 import icon from '@icons/icon';
@@ -11,10 +15,38 @@ import Map from '@components/Common/Map';
 import './GameMap.css';
 import LocationImage from '@components/Common/LocationImage';
 
+// const getData = async (component: any) => {
+//   const ReactDOMServer = (await import('react-dom/server')).default;
+//   const staticMarkup = renderToStaticMarkup(component);
+//   // const staticMarkup = ReactDOMServer.renderToStaticMarkup(component);
+//   return staticMarkup;
+// };
+
+// function renderToStaticMarkup(element: any): any {
+//   return new Promise((resolve, reject) => {
+//     let html = '';
+//     const writableStream = new Writable({
+//       write(chunk: any, encoding: any, callback: any) {
+//         html += chunk;
+//         callback();
+//       },
+//     });
+
+//     writableStream.on('finish', () => {
+//       resolve(html);
+//     });
+
+//     writableStream.on('error', reject);
+
+//     renderToPipeableStream(element).pipe(writableStream);
+//   });
+// }
+
 const GameMap: React.FC<GameMapProps> = (props: GameMapProps) => {
     const MapClickHandler: React.FC = () => {
         const map = useMapEvents({
-            click: (e) => {
+            click: async (e) => {
+            // click: (e) => {
                 if (!props.locationData || props.guessed || !props.mapData) {
                     // Location data hasn't been set yet.
                     return;
@@ -39,12 +71,24 @@ const GameMap: React.FC<GameMapProps> = (props: GameMapProps) => {
                 ).addTo(map);
 
                 // Create the popup showing the correct location.
-                // const popupContainer = document.createElement('div');
-                // ReactDOM.render(<LocationImage area={props.area} id={props.locationData.id}/>, popupContainer);
 
-                const correctPopup = L.popup({}).setContent(
-                    `<img src="/areas/${props.locationData.area}/locations/${props.locationData.filename}" alt="${props.locationData.name}" width="200" height="200">`,
-                );
+                // const popupContainer = document.createElement('div');
+
+                // ReactDOM.render(<LocationImage locationData={props.locationData} size='100px'/>, popupContainer);
+                // createPortal(<LocationImage locationData={props.locationData} size='100px'/>, popupContainer);(<LocationImage locationData={props.locationData} size='100px'/>, popupContainer);
+
+                // const locationDataPopup = ReactDOMServer.renderToString(<LocationImage locationData={props.locationData} size='100px'/>);
+                // const locationDataPopup = await renderToStaticMarkup(<LocationImage locationData={props.locationData} size='100px'/>);
+
+                // const locationDataPopup = await getData(<LocationImage locationData={props.locationData} size='100px'/>);
+
+                const locationDataPopup = `<img src="/areas/${props.locationData.area}/locations/${props.locationData.filename}" alt="${props.locationData.name}" width="200px" height="200px">`;
+
+                const correctPopup = L.popup({}).setContent(locationDataPopup);
+                // const correctPopup = L.popup({}).setContent(popupContainer);
+                // const correctPopup = L.popup({}).setContent(
+                //     `<img src="/areas/${props.locationData.area}/locations/${props.locationData.filename}" alt="${props.locationData.name}" width="200" height="200">`,
+                // );
                 correctMarker.bindPopup(correctPopup).openPopup();
 
                 const latlngs = [
