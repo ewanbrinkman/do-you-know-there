575d9c4e9202ed444109ee64387f216cbe83d8c4
Ewan Brinkman <ewanbrinkman@gmail.com>
2023-11-22 18:26:18 -0800

    feat: add default pages
    Add default page types for Next.js, for example an error 404 page. That
    way, the default error 404 page isn't used.

---
+++ src/app/loading.tsx
@@ -0,0 +1,18 @@
+'use client';
+import React from 'react';
+import useDeveloperMessage from '@hooks/useDeveloperMessage';
+import Container from '@components/Common/Container';
+
+const Home: React.FC = () => {
+    useDeveloperMessage();
+
+    return (
+        <div className="flex flex-grow flex-col items-center justify-center p-8 space-y-8 bg-gradient-to-b from-white to-primary-color">
+            <Container className="mx-16">
+                <h1 className="text-3xl font-bold">Loading...</h1>
+            </Container>
+        </div>
+    );
+};
+
+export default Home;
