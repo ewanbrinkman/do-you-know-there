50204a1d6bfda21638d7733bd32ab869c186acac
Ewan Brinkman <56836361+ewanbrinkman@users.noreply.github.com>
2023-11-22 22:05:06 -0800

    feat: add map zoom settings for large screen size (#50)
    * feat: add map zoom settings for large screen size
    
    Make it so the map zoom looks good on large screens too. Currently,
    medium and small screens are handled.
    
    * fix: add large screen to raw map data interface
    
    Add the new large screen type to the RawMapData interface.

--- src/app/types/data/ScreenSize.ts
+++ src/app/types/data/ScreenSize.ts
@@ -1,6 +1,7 @@
 enum ScreenSize {
     Small = 'smallScreen',
     Base = 'baseScreen',
+    Large = 'largeScreen',
 }
 
 export default ScreenSize;
--- src/app/utils/getters/getMapData.ts
+++ src/app/utils/getters/getMapData.ts
@@ -2,10 +2,18 @@ import MapData from '@typings/data/MapData';
 import ProcessedMapData from '@typings/data/ProcessedMapData';
 import ScreenSize from '@typings/data/ScreenSize';
 
-export default function convertProcessedMapDataToMapData(
+export default function getMapData(
     processedMapData: ProcessedMapData,
-    screenSize: ScreenSize,
+    screenWidth: number,
 ): MapData {
+    let screenSize: ScreenSize = ScreenSize.Small;
+
+    if (screenWidth >= 3100) {
+        screenSize = ScreenSize.Large;
+    } else if (screenWidth >= 800) {
+        screenSize = ScreenSize.Base;
+    }
+
     return {
         area: processedMapData.area,
         center: {
